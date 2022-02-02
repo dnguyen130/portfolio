c6651a48cb0ba6c187763c86e968cd54fb96b6dd
Danny Nguyen <dnguyen130@my.bcit.ca>
2022-02-01 22:48:16 -0800

    added alts to images

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -9,12 +9,12 @@ const NavBar = ({
 
   return(
     <div className="navbar">
-      <Image src="/logo.svg" height={100} width={100} />
-      <Image src="/home.svg" height={60} width={60} />
-      <Image src="/about.svg" height={60} width={60} />
-      <Image src="/toolkit.svg" height={60} width={60} />
-      <Image src="/portfolio.svg" height={60} width={60} />
-      <Image src="/contact.svg" height={60} width={60} />
+      <Image src="/logo.svg" alt="Logo"  height={100} width={100} />
+      <Image src="/home.svg" alt="Home" height={60} width={60} />
+      <Image src="/about.svg" alt="About" height={60} width={60} />
+      <Image src="/toolkit.svg" alt="Toolkit" height={60} width={60} />
+      <Image src="/portfolio.svg" alt="Portfolio" height={60} width={60} />
+      <Image src="/contact.svg" alt="Contact" height={60} width={60} />
 
     </div>
   )
