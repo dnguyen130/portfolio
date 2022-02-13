a23116acd857b283e4731c2df73dc7eaaf66d724
Danny Nguyen <dnguyen130@my.bcit.ca>
2022-02-13 15:20:53 -0800

    deleted downchevron for now

--- components/DownChevron/index.js
+++
@@ -1,5 +0,0 @@
-import { motion } from "framer-motion"
-import styled from "styled-components"
-
-
-<HiChevronDoubleDown color={site_theme[theme].text} size="60px" style={{position: "absolute", bottom: 0}} />
